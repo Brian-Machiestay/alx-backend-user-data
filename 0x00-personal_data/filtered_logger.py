ba992a6e815e30d5d064f1b616568cac7348ed69
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-02 21:12:48 +0000

    create a filtered logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""filter and obfuscate user info"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """filter user data"""
+    m = message
+    for field in fields:
+        b = re.search(r'{}=.*'.format(field), m)
+        m = re.sub(b.group().split(';')[0],
+                   '{}={}'.format(field, redaction), m)
+    return m
