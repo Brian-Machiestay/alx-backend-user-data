6f46732388cadf1623781d377cf7458adc2e8656
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-07 22:58:23 +0000

    correct type annonation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -42,7 +42,7 @@ def forbidden(error) -> str:
 
 
 @app.before_request
-def before_request():
+def before_request() -> str:
     """execute this function before any other request"""
     if auth is None:
         return
