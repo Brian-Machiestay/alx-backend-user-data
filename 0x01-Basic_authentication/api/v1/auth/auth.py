d3a298c53f8f3d3ba5569ab51914b66cc8333e6b
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-07 21:10:21 +0000

    remove excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,16 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """check the paths that require authentication"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        for pat in excluded_paths:
+            if pat == path:
+                return False
+            if pat.startswith(path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """checks the authorizaton header"""
