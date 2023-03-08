bb07012a7e9d50fe6c1a84c597e7645ac96419ff
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-08 05:30:00 +0000

    check user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """implement a basic authentication"""
 from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
 import base64
 
 
@@ -44,3 +46,20 @@ class BasicAuth(Auth):
             return None, None
         credentials = decoded_base64_authorization_header.split(':')
         return credentials[0], credentials[1]
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """"return a user object based on email and password"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        objs = User.search({'email': user_email})
+        if objs is None or objs == []:
+            return None
+        actual_user = None
+        for use in objs:
+            if use.is_valid_password(user_pwd):
+                actual_user = use
+        return actual_user
