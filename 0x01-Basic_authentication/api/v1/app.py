8f72bce9a54f012d6f0d2065e500d7e7dd2aaeb4
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-08 03:49:22 +0000

    check this point

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,6 +15,8 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+if os.environ['AUTH_TYPE'] == 'auth':
+    auth = Auth()
 
 
 @app.errorhandler(404)
