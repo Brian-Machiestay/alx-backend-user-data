ba9ef56e14189e664aa11329269ea95f06331c44
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-09 22:06:38 +0000

    create and return a session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 """create a session_auth class"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """the auth class to handle session authentication"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """function to create a session"""
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = uuid.uuid4()
+        SessionAuth.user_id_by_session_id[str(session_id)] = user_id
+        return session_id
