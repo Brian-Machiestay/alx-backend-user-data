ba9e55dbc8291a2d7957533bc732822dfc9ffd91
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-16 15:12:25 +0000

    create user registration endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,16 +2,30 @@
 """python flask app defines routes to this application"""
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', strict_slashes=False)
 def root():
     """return the homepage of this application"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """register a user on this application"""
+    email = request.form.get('email', None)
+    pwd = request.form.get('password', None)
+    try:
+        usr = AUTH.register_user(email, pwd)
+        return jsonify({"email": usr.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
