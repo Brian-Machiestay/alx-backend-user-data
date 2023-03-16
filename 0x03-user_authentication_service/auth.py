46ce934a1038662df8b619ed3ed5beec1b72c6c7
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-16 19:37:41 +0000

    implement generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,12 +7,19 @@ from base64 import standard_b64encode
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
     """"implement a method that returns bytes password"""
     pwd = password.encode('utf-8')
     return bcrypt.hashpw(pwd, bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """generate uuid and return a str repr"""
+    uid = uuid4()
+    return str(uid)
 
 
 class Auth:
