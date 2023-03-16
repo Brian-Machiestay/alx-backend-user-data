b8f158c976131601c71f204f822f9a45cd27dd84
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-16 14:17:36 +0000

    create a simple flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""python flask app defines routes to this application"""
+
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """return the homepage of this application"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
