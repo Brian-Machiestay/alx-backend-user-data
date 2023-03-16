73ce8c7f5842dd8ca1fc11f52662df775bd1ad37
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-16 19:30:06 +0000

    check valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,3 +31,12 @@ class Auth:
             pwd = _hash_password(password)
             user = self._db.add_user(email, pwd)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check that this user is valid"""
+        try:
+            usr = self._db.find_user_by(email=email)
+            pwd = password.encode('utf-8')
+            return bcrypt.checkpw(pwd, usr.hashed_password)
+        except NoResultFound:
+            return False
