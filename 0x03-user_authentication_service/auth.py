f640fc1780187ea631d3d81dae6cde6e81c7bf3e
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-16 22:27:55 +0000

    implement update password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,3 +83,12 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """updates the password of this user"""
+        try:
+            usr = self._db.find_user_by(reset_token=reset_token)
+            hpw = _hash_password(password)
+            self._db.update_user(usr.id, hashed_password=hpw, reset_token=None)
+        except NoResultFound:
+            raise ValueError
