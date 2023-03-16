5bb00d8a4e2e3c412ff8fc30e89cf281a247033c
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-16 20:23:14 +0000

    permit session login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """python flask app defines routes to this application"""
 
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -26,6 +26,20 @@ def users():
         return jsonify({"email": usr.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """log this user in using the session"""
+    email = request.form.get('email', None)
+    pwd = request.form.get('password', None)
+    correct_details = AUTH.valid_login(email, pwd)
+    if not correct_details:
+        abort(401)
+    sess_id = AUTH.create_session(email)
+    resp = make_response(jsonify({"email": email, "message": "logged in"}))
+    resp.set_cookie("session_id", sess_id)
+    return resp
 
 
 if __name__ == "__main__":
